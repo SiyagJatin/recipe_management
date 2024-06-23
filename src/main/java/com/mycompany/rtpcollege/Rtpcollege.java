4f8dc81754618fea74acfaffdee5c9101bcfe9eb
SiyagJatin <siyagjatin@gmail.com>
2024-06-23 23:56:57 +0530

    commit message

---
+++ src/main/java/com/mycompany/rtpcollege/Rtpcollege.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.rtpcollege;
+
+/**
+ *
+ * @author Jatin
+ */
+public class Rtpcollege {
+
+    public static void main(String[] args) {
+        login_page loginframe = new login_page();
+        loginframe.setVisible(true);
+    }
+}
